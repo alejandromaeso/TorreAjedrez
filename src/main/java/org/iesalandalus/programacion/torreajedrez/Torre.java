6cf0edb088dbf50bdb494531186bfe7defbb451e
Alejandrico <Alejandrico@DESKTOP-DUKH0VE>
2021-11-09 17:47:13 +0100

    Creo un constructor para la clase torre que acepta como parámetro el color y que creará una torre en 1h si es blanca o 8h si es negra.

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
@@ -23,9 +23,24 @@ public class Torre {
 	}
 	
 	//Constructor por defecto para crear una torre negra en '8h'
-	public Torre(Color color) {
+	public Torre() {
 		color = Color.NEGRO;
 		posicion = new Posicion(8,'h');
 		
+	}
+	
+	//Constructor para la clase que acepte como parámetro el color y que creará una torre de dicho color '1h' blanca // '8h' negra
+	
+	public Torre(Color color) {
+		this.color = color;
+		if (color == Color.BLANCO) {
+			posicion = new Posicion(1, 'h');
+		}
+		if (color == Color.NEGRO) {
+			posicion = new Posicion(8, 'h');
+		}
+		if (color == null) {
+			throw new IllegalArgumentException("ERROR: No se puede asignar un color nulo.");
+		}
 	}
 }
