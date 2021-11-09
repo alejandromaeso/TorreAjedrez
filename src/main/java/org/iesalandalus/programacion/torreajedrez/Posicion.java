ff887ae122ea3299d02057a8b886c472351db085
Alejandrico <Alejandrico@DESKTOP-DUKH0VE>
2021-11-09 15:47:10 +0100

    Creo los métodos get y set para los atributos. Añado "Ifs" para tener en cuenta el rango de las filas y de las columnas y si no se cumple, que salte una excepción del tipo IllegalArgumentException.

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
@@ -7,5 +7,34 @@ public class Posicion {
 	public int fila;
 	public char columna;
 	
-}
+	//Metodos
 	
+	public int getFila() {
+		return fila;
+	}
+	
+	public void setFila(int fila) {
+		if (fila < 1 || fila > 8)
+		{
+			throw new IllegalArgumentException("ERROR: Fila no válida.");
+		}
+		this.fila = fila;
+	}
+	
+	public char getColumna() {
+		return columna;
+	}
+	
+	public void setColumna(char columna) {
+		if (columna != 'a' || columna != 'b' || columna != 'c' || columna != 'd' || columna != 'e' || columna != 'f' || columna != 'g' || columna != 'h' ) 
+		{
+			throw new IllegalArgumentException("ERROR: Columna no válida");
+		}
+		this.columna = columna;
+	}
+	
+	
+	//Ejercicio 6 - En caso de que la posición no sea la correcta no deben modificarse los atributos. Realiza un commit.
+	
+}
+
