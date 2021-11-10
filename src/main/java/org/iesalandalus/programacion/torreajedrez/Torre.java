56a665b4927e42e90358a3c7dc6163e4c84216ac
Alejandrico <Alejandrico@DESKTOP-DUKH0VE>
2021-11-10 20:28:04 +0100

    Creo un constructor para la clase Torre que acepta como parámetros el color y la columna inicial. La columna inicial en caso de no ser válida, lanza una excepción. La torre la creará en "1-A" de ser blanca o en "8-H" en caso de ser negra.

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
@@ -1,36 +1,39 @@
 package org.iesalandalus.programacion.torreajedrez;
 
 public class Torre {
-	
-	//Atributos
-	
+
+	// Atributos
+
 	private Color color;
 	private Posicion posicion;
 
-	//Métodos
-	
+	// Métodos
+
 	public Color getColor() {
 		return color;
 	}
+
 	public void setColor(Color color) {
 		this.color = color;
 	}
+
 	public Posicion getPosicion() {
 		return posicion;
 	}
+
 	public void setPosicion(Posicion posicion) {
 		this.posicion = posicion;
 	}
-	
-	//Constructor por defecto para crear una torre negra en '8h'
+
+	// Constructor por defecto para crear una torre negra en '8h'
 	public Torre() {
 		color = Color.NEGRO;
-		posicion = new Posicion(8,'h');
-		
+		posicion = new Posicion(8, 'h');
 	}
-	
-	//Constructor para la clase que acepte como parámetro el color y que creará una torre de dicho color '1h' blanca // '8h' negra
-	
+
+	// Constructor para la clase que acepte como parámetro el color y que creará una
+	// torre de dicho color '1h' blanca // '8h' negra
+
 	public Torre(Color color) {
 		this.color = color;
 		if (color == Color.BLANCO) {
@@ -43,4 +46,24 @@ public class Torre {
 			throw new IllegalArgumentException("ERROR: No se puede asignar un color nulo.");
 		}
 	}
+
+	// Constructor que acepte como parámetros el color y la columna inicial, 
+	// Crea la torre en "1-A" si es blanca y en "8-N" si es negra
+
+	public Torre(Color color, char columna) {
+		this.color = color;
+		if (columna == 'a' || columna == 'h') {
+			if (this.color == Color.BLANCO) {
+				posicion = new Posicion(1, columna);
+			} else {
+				posicion = new Posicion(8, columna);
+			}
+		} else {
+			throw new IllegalArgumentException("ERROR: Columna no válida.");
+		}
+		if (this.color == null) {
+			throw new IllegalArgumentException("ERROR: No se puede asignar un color nulo.");
+		}
+	}
+
 }
